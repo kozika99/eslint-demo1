df3a31bf227126cf7fa0402cf65676962af2410b
Tamás Kozák <minikozi99@gmail.com>
2020-08-12 15:23:25 +0200

    'Working eslint and prettier'

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ console.log("hello world");
 
 // eslint-disable-next-line no-unused-vars
 let a = 5;
+// eslint-disable-next-line no-unused-vars
+let b = 3;
 
 // Helper function to print to the screen.
 function print(line) {
